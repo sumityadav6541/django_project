158d1340e7e88c65cf88e3c1f9ed2110fd9497cc
Sumit Yadav <sumityadav6541@gmail.com>
2016-08-12 18:41:48 +0530

    added 404 response page/method. newboston#16

--- music/views.py
+++ music/views.py
@@ -1,12 +1,15 @@
-from django.http import HttpResponse
+from django.http import Http404
 from django.shortcuts import render
 from .models import Album
 
 def index(request):
     all_albums = Album.objects.all()
-    context = { 'all_albums':all_albums}
-    return render(request, 'music/index.html',context)
+    return render(request, 'music/index.html', {'all_albums':all_albums})
 
 def detail(request, album_id):
-    html = "<h2>Details for Album id: "+str(album_id)+ "</h2>"
-    return HttpResponse(html)
+    try:
+        album = Album.objects.get(pk=album_id)
+    except Album.DoesNotExist:
+        raise Http404("Album does not exist.")
+
+    return render(request,'music/details.html', {'album':album} )
