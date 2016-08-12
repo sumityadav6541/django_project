b33f00b7d3a349df4c66f17cf2d2d750a63b8945
Sumit Yadav <sumityadav6541@gmail.com>
2016-08-12 12:33:48 +0530

    added template based view generation for music app. newboston tutorial#14

--- music/views.py
+++ music/views.py
@@ -1,13 +1,14 @@
 from django.http import HttpResponse
+from django.template import loader
 from .models import Album
 
 def index(request):
     all_albums = Album.objects.all()
-    html = "<h4>This is the music app homepage</h4>"
-    for album in all_albums:
-        url = '/music/'+ str(album.id)+'/'
-        html+=  '<a href="'+url+'">'+album.album_title+'</a><br>'
-    return HttpResponse(html)
+    template = loader.get_template('music/index.html')
+    context = {
+        'all_albums':all_albums,
+    }
+    return HttpResponse(template.render(context,request))
 
 def detail(request, album_id):
     html = "<h2>Details for Album id: "+str(album_id)+ "</h2>"
