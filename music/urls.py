ab40318b9d45cecde76aedfa80ec09f121a679fe
Sumit <sumityadav6541@gmail.com>
2016-08-23 01:38:19 +0530

    added choose favorite song option in music app.

--- music/urls.py
+++ music/urls.py
@@ -8,5 +8,8 @@ urlpatterns = [
     url(r'^$',views.index, name='index'),
 
     #/music/71/
-    url(r'^(?P<album_id>[0-9]+)/$',views.detail,name='detail')
+    url(r'^(?P<album_id>[0-9]+)/$',views.detail,name='detail'),
+
+    #/music/71/favorite
+    url(r'^(?P<album_id>[0-9]+)/favorite/$',views.favorite,name='favorite')
 ]
